f8a30747476cc82eaa0a48355f11ecc50fab8ec4
Ritu Kumar <the.ritukumar@gmail.com>
2026-03-25 23:29:10 +0530

    Time: 12 ms (45.68%) | Memory: 130.4 MB (33.09%) - LeetSync

---
+++ 3849-equal-sum-grid-partition-i/equal-sum-grid-partition-i.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    typedef long long ll;
+    bool canPartitionGrid(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        ll total = 0;
+        vector<ll> rowSum(m, 0);
+        vector<ll> colSum(n, 0);
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                total += grid[i][j];
+                // rowSum;
+                rowSum[i] += grid[i][j];
+                // colSum
+                colSum[j] += grid[i][j];
+            }
+        }
+ 
+
+        ll upper = 0;
+        for (int i = 0; i < m - 1; i++) {
+            upper += rowSum[i];
+            if (upper == total - upper) {
+                return true;
+            }
+        }
+
+        ll left = 0;
+        for (int j = 0; j < n - 1; j++) {
+            left += colSum[j];
+            if (left == total - left) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+};
